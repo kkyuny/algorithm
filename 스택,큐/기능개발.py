60e3ff1e96c0f890fb6f12f16aeb9799c2b68174
kyun <88278485+kkyuny@users.noreply.github.com>
2022-01-18 18:58:25 +0900

    Update 기능개발.py

--- 스택,큐/기능개발.py
+++ 스택,큐/기능개발.py
@@ -2,27 +2,26 @@ def solution(progresses, speeds):
     answer = []  
        
     i = 0    
-    while progresses:
-        j = 0         
-        while j < len(progresses):
-            l = 0
-            
-            for a in range(len(progresses)):
-                if progresses[a] < 100:
-                    progresses[a] += speeds[a]                
+    while progresses:    
+        l = 0        
+        for a in range(len(progresses)):
+            if progresses[a] < 100:
+                progresses[a] += speeds[a]                
+        
+        print(progresses)
 
-            while progresses[0] >= 100:                       
-                progresses.pop(0)
-                speeds.pop(0)                
-                l += 1
+        while progresses[0] >= 100:                       
+            progresses.pop(0)
+            speeds.pop(0)                
+            l += 1
 
-                if len(progresses) == 0:
-                    answer.append(l)
-                    break                     
-                elif progresses[0] < 100:
-                    answer.append(l)
-                    break                            
-            j += 1               
-            if l > 0:
-                break                                         
+            if len(progresses) == 0:
+                answer.append(l)
+                break                     
+            elif progresses[0] < 100:
+                answer.append(l)
+                break       
+
+        print(answer)                     
+                                              
     return answer
